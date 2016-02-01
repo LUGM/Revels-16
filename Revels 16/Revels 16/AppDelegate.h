f5ec2cf4372fcffd916b1f4dc3a5e92eda8a1132
avikantz <avikantsainidbz@gmail.com>
2016-02-01 11:09:11 +0530

    Setting up.

---
+++ Revels 16/Revels 16/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Revels 16
+//
+//  Created by Avikant Saini on 2/1/16.
+//  Copyright © 2016 Dark Army. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Revels 16/Revels 16/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Revels 16
+//
+//  Created by Avikant Saini on 2/1/16.
+//  Copyright © 2016 Dark Army. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
